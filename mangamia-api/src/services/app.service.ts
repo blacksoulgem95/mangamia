b7a1e24fff8fd71eb9cec2226a108c2ae0bd9432
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2025-01-03 14:57:05 +0100

    init

--- mangamia-api/src/services/app.service.ts
+++ mangamia-api/src/services/app.service.ts
@@ -2,7 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getBanner(): string {
+    return `
+███╗   ███╗ █████╗ ███╗   ██╗ ██████╗  █████╗     ███╗   ███╗██╗ █████╗ ██╗
+████╗ ████║██╔══██╗████╗  ██║██╔════╝ ██╔══██╗    ████╗ ████║██║██╔══██╗██║
+██╔████╔██║███████║██╔██╗ ██║██║  ███╗███████║    ██╔████╔██║██║███████║██║
+██║╚██╔╝██║██╔══██║██║╚██╗██║██║   ██║██╔══██║    ██║╚██╔╝██║██║██╔══██║╚═╝
+██║ ╚═╝ ██║██║  ██║██║ ╚████║╚██████╔╝██║  ██║    ██║ ╚═╝ ██║██║██║  ██║██╗
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝╚═╝  ╚═╝╚═╝
+
+Manga Mia! API Platform
+
+Copyright (C) 2024 Sofia Vicedomini (github.com/blacksoulgem95)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see https://www.gnu.org/licenses/ 
+
+`;
   }
 }
